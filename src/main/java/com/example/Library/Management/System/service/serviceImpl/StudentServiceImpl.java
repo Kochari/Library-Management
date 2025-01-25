671936b12555c74811a5aa0e5855223bc3e88977
Kochari <koceri.agalarov@gmail.com>
2025-01-25 12:03:07 +0400

    serviceImpl

--- src/main/java/com/example/Library/Management/System/service/serviceImpl/StudentServiceImpl.java
+++ src/main/java/com/example/Library/Management/System/service/serviceImpl/StudentServiceImpl.java
@@ -8,9 +8,13 @@ import com.example.Library.Management.System.repository.StudentRepository;
 import com.example.Library.Management.System.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,12 +25,25 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<StudentResponse> getAllStudents(int page, int size) {
-        return List.of();
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Student> students = studentRepository.findAll(pageable);
+        if (students.isEmpty()) {
+            throw new NotFoundException("Student not found");
+        }else {
+            return studentRepository
+                    .findAll()
+                    .stream()
+                    .map(studentMapper::studentToStudentResponse)
+                    .collect(Collectors.toList());
+        }
     }
 
     @Override
     public StudentResponse getStudentById(Long id) {
-        return null;
+        Student student = studentRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("Student not found")
+        );
+        return studentMapper.studentToStudentResponse(student);
     }
 
     @Override
@@ -46,6 +63,7 @@ public class StudentServiceImpl implements StudentService {
         _student.setPhone(student.getPhone());
         _student.setFIN(student.getFIN());
         _student.setOrders(student.getOrders());
+        studentRepository.save(_student);
 
     }
 
